6e45f1952531fc5b19ce56565d2bda359e188f8b
Ostap Blystsiv <ostap.blystsiv@gmail.com>
2023-06-15 02:06:29 +0300

    test commit

--- client/src/IntroductionPage/IntroductionPage.js
+++ client/src/IntroductionPage/IntroductionPage.js
@@ -54,6 +54,7 @@ const IntroductionPage = ({ setIsRoomHostAction }) => {
         </div>
       )}
       {renderButton()}
+      <h2>123123</h2>
       <div className="introduction_page_panel">
         <img src={logo} className="introduction_page_image" alt="Logo" />
         <ConnectingButtons />
