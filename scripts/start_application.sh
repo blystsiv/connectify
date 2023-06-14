453b0cc34d206479182d7ab07f69f4577418d7e7
Ostap Blystsiv <ostap.blystsiv@gmail.com>
2023-06-14 23:30:57 +0300

    modfied starting script

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Starting frontend..."
-cd client/ && npm install && npm start && cd ../
+cd ../client/ && npm install && npm start && cd ../scripts
 echo "Frontend has been started"
 
 echo "Starting backend..."
-cd server/ && npm install && node server.js && cd ../
+cd ../server/ && npm install && node server.js
 echo "Backend has been started"
