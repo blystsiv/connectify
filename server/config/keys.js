d83e65f16b14f162618535a578f24068862d9cba
Ostap Blystsiv <ostap.blystsiv@gmail.com>
2023-06-21 14:28:22 +0300

    Clear some unnecesary files and update credentials

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,12 +1,12 @@
 module.exports = {
     google: {
-        clientID: '83312942729-p3vr213nkradtmja03nume2fcm3j2iat.apps.googleusercontent.com',
-        clientSecret: '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'
+        clientID: '709679411732-jhesubn0u147ij1uiup7csn94m780m23.apps.googleusercontent.com',
+        clientSecret: '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'
     },
     session: {
         secretWord: 'testtest'
     },
     mongodb: {
-        dbURI: 'mongodb+srv://root:root@cluster0.nrkuyk7.mongodb.net/?retryWrites=true&w=majority'
+        dbURI: 'mongodb+srv://gstratiyj:5pkx8fts@cluster0.ruatl0a.mongodb.net/?retryWrites=true&w=majority'
     }
 };
